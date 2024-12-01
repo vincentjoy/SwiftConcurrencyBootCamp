24f4b8e90cbb95530b4d908785cccc55913512fa
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-01 09:28:48 +0530

    Initial Commit

---
+++ SwiftConcurrencyBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Vincent Joy on 01/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConcurrencyBootCamp/SwiftConcurrencyBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootCampApp.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Vincent Joy on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
