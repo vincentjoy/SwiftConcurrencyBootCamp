9cd01d8a4e348d8769fa74fa05095b4cbc95342d
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-19 19:40:28 +0530

    wrote the logic to fetch multiple images parallel'y using async let

--- SwiftConcurrencyBootCamp/Async/TaskGroupBootcamp.swift
+++ SwiftConcurrencyBootCamp/Async/TaskGroupBootcamp.swift
@@ -1,7 +1,46 @@
 import SwiftUI
 
+class TaskGroupBootcampDataManager {
+    
+    func fetchImageswithAsyncLet() async throws -> [UIImage] {
+        async let fetchImage1 = fetchImage(urlString: "https://picsum.photos/200")
+        async let fetchImage2 = fetchImage(urlString: "https://picsum.photos/200")
+        async let fetchImage3 = fetchImage(urlString: "https://picsum.photos/200")
+        async let fetchImage4 = fetchImage(urlString: "https://picsum.photos/200")
+        async let fetchImage5 = fetchImage(urlString: "https://picsum.photos/200")
+        
+        let (image1, image2, image3, image4, image5) = await (try fetchImage1, try fetchImage2, try fetchImage3, try fetchImage4, try fetchImage5)
+        
+        return [image1, image2, image3, image4, image5]
+    }
+    
+    func fetchImage(urlString: String) async throws -> UIImage {
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            if let image = UIImage(data: data) {
+                return image
+            } else {
+                throw URLError(.badURL)
+            }
+        } catch {
+            throw error
+        }
+    }
+}
+
 class TaskGroupBootcampViewModel: ObservableObject {
     @Published var images: [UIImage] = []
+    let manager = TaskGroupBootcampDataManager()
+    
+    func getImages() async {
+        if let images = try? await manager.fetchImageswithAsyncLet() {
+            self.images.append(contentsOf: images)
+        }
+    }
 }
 
 struct TaskGroupBootcamp: View {
@@ -22,6 +61,9 @@ struct TaskGroupBootcamp: View {
                 }
             }
             .navigationTitle("Task Group 🥳")
+            .task {
+                await viewModel.getImages()
+            }
         }
     }
 }
