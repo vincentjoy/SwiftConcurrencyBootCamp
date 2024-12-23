f775a9a35ba15d2654759046431626cbb528b187
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-23 15:13:59 +0530

    mocekd a race condition

--- SwiftConcurrencyBootCamp/Async/ActorBootCamp.swift
+++ SwiftConcurrencyBootCamp/Async/ActorBootCamp.swift
@@ -1,7 +1,20 @@
 import SwiftUI
 
+class MyDataManager {
+    static let instance = MyDataManager()
+    private init() {}
+    var data: [String] = []
+    
+    func getRandomData() -> String? {
+        self.data.append(UUID().uuidString)
+        print(Thread.current)
+        return data.randomElement()
+    }
+}
+
 struct HomeView: View {
     
+    let manager = MyDataManager.instance
     @State private var text: String = ""
     let timer = Timer.publish(every: 0.1, tolerance: nil, on: .main, in: .common, options: nil).autoconnect()
     
@@ -12,15 +25,37 @@ struct HomeView: View {
                 .font(.headline)
         }
         .onReceive(timer) { _ in
-            
+            DispatchQueue.global(qos: .background).async {
+                if let data = manager.getRandomData() {
+                    DispatchQueue.main.async {
+                        self.text = data
+                    }
+                }
+            }
         }
     }
 }
 
 struct BrowseView: View {
+    
+    let manager = MyDataManager.instance
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.01, tolerance: nil, on: .main, in: .common, options: nil).autoconnect()
+    
     var body: some View {
         ZStack {
             Color.yellow.opacity(0.8).ignoresSafeArea()
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            DispatchQueue.global(qos: .default).async {
+                if let data = manager.getRandomData() {
+                    DispatchQueue.main.async {
+                        self.text = data
+                    }
+                }
+            }
         }
     }
 }
