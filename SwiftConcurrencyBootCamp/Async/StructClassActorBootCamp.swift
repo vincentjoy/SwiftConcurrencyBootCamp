35b025d173eae409353bb6a5591ef61b955b0d30
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-23 10:00:14 +0530

    completed the first struct test

--- SwiftConcurrencyBootCamp/Async/StructClassActorBootCamp.swift
+++ SwiftConcurrencyBootCamp/Async/StructClassActorBootCamp.swift
@@ -13,8 +13,29 @@ struct StructClassActorBootCamp: View {
     StructClassActorBootCamp()
 }
 
+struct MyStruct {
+    var title: String
+}
+
 extension StructClassActorBootCamp {
+    
     private func runTest() {
         print("Test Started")
+        structTest1()
+    }
+    
+    private func structTest1() {
+        let objectA = MyStruct(title: "Starting Title")
+        print("ObjectA: \(objectA.title)")
+        
+        print("Pass the values of objectA to objectB")
+        var objectB = objectA
+        print("ObjectB: \(objectB.title)")
+        
+        objectB.title = "New Title"
+        print("ObjectB title changed")
+        
+        print("ObjectA title: \(objectA.title)")
+        print("ObjectB title: \(objectB.title)")
     }
 }
