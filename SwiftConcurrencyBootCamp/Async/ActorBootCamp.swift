160f754c6664e70f7f857e740b51503939195b39
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-23 14:57:00 +0530

    ui setup done

---
+++ SwiftConcurrencyBootCamp/Async/ActorBootCamp.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var text: String = ""
+    let timer = Timer.publish(every: 0.1, tolerance: nil, on: .main, in: .common, options: nil).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.gray.opacity(0.8).ignoresSafeArea()
+            Text(text)
+                .font(.headline)
+        }
+        .onReceive(timer) { _ in
+            
+        }
+    }
+}
+
+struct BrowseView: View {
+    var body: some View {
+        ZStack {
+            Color.yellow.opacity(0.8).ignoresSafeArea()
+        }
+    }
+}
+
+struct ActorBootCamp: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            BrowseView()
+                .tabItem {
+                    Label("Browse", systemImage: "magnifyingglass")
+                }
+        }
+    }
+}
+
+#Preview {
+    ActorBootCamp()
+}
