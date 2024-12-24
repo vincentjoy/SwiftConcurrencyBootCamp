992ba5cec7e895e6dd1dd44ccf60619af772785a
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-24 08:18:44 +0530

    added the sendable class and struct to pass into an async thread-safe  enviornment

--- SwiftConcurrencyBootCamp/Async/SendableBootCamp.swift
+++ SwiftConcurrencyBootCamp/Async/SendableBootCamp.swift
@@ -4,8 +4,30 @@ import SwiftUI
 
 actor CurrentUserManager {
     
-    func updateDataBase() {
-        
+    func updateDataBase(userInfo: MyClassUserInfo) {
+        // since MyClassUserInfo is Sendable, it will be thread safe and so can be passed into an actor
+        // Try to use Struct or some value types if possible (struct still needs to be marked as sendable)
+        // If class, mark it as unchecked sendable and handle the thread safety by using serial queue or lock or semaphore
+    }
+}
+
+struct MyUserInfo: Sendable {
+    var name: String
+}
+
+final class MyClassUserInfo: @unchecked Sendable { // Sendable
+    
+    var name: String
+    let queue = DispatchQueue(label: "com.app.MyClassUserInfo")
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func updateName(name: String) {
+        queue.async {
+            self.name = name
+        }
     }
 }
 
@@ -14,7 +36,8 @@ class SendableBootCampViewModel: ObservableObject {
     let manager = CurrentUserManager()
     
     func updateCurrentUserInfo() async {
-        await manager.updateDataBase()
+        let info = MyClassUserInfo(name: "Vincent")
+        await manager.updateDataBase(userInfo: info)
     }
 }
 
