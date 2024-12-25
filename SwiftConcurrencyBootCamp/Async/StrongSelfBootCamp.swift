b431d2992171b95152a01e4fd8d3cfd9f7f5edf2
Vincent Joy <vincentjoyhere@gmail.com>
2024-12-25 15:45:14 +0530

    added the cancellable tasks and weak reference examples

--- SwiftConcurrencyBootCamp/Async/StrongSelfBootCamp.swift
+++ SwiftConcurrencyBootCamp/Async/StrongSelfBootCamp.swift
@@ -10,13 +10,52 @@ final class StrongSelfDataService {
 }
 
 class StrongSelfBootCampViewModel: ObservableObject {
+    
     @Published var data: String = "Some title"
     let dataService = StrongSelfDataService()
+    private var someTask: Task<Void, Never>?
     
+    // This implies strong reference
     func updateData() {
         Task {
             data = await dataService.getData()
         }
+    }
+    
+    // This implies strong reference
+    func updateData2() {
+        Task {
+            self.data = await self.dataService.getData()
+        }
+    }
+    
+    // This implies strong reference
+    func updateData3() {
+        Task { [self] in
+            self.data = await self.dataService.getData()
+        }
+    }
+    
+    // This is a weak reference
+    func updateData4() {
+        Task { [weak self] in
+            if let data = await self?.dataService.getData() {
+                self?.data = data
+            }
+        }
+    }
+    
+    // We don't need to manage weak/strong
+    // Because we can manage the Task
+    func updateData5() {
+        someTask = Task {
+            self.data = await self.dataService.getData()
+        }
+    }
+    
+    func cancelTask() {
+        someTask?.cancel()
+        someTask = nil
     }
 }
 
@@ -28,6 +67,9 @@ struct StrongSelfBootCamp: View {
         Text(viewModel.data)
             .onAppear {
                 viewModel.updateData()
+            }
+            .onDisappear {
+                viewModel.cancelTask()
             }
     }
 }
