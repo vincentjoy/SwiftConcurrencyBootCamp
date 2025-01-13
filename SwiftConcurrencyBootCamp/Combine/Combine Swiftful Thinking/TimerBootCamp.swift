ba92eaa305a8e6a8469d4e6c9e331c3008f728ce
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-13 16:14:15 +0530

    animations with timer added

--- SwiftConcurrencyBootCamp/Combine/Combine Swiftful Thinking/TimerBootCamp.swift
+++ SwiftConcurrencyBootCamp/Combine/Combine Swiftful Thinking/TimerBootCamp.swift
@@ -17,15 +17,22 @@ struct TimerBootCamp: View {
     }
     */
     
+    
     // Count down
     /*
     @State private var counter: Int = 10
     @State private var finishedText: String?
     */
     
+    
     // Count down to date
+    /*
     @State private var timeRemaining: String = ""
     private let futureData: Date = Calendar.current.date(byAdding: .day, value: 1, to: Date()) ?? Date()
+    */
+    
+    // Animation Counter
+    @State private var count: Int = 0
     
     var body: some View {
         ZStack {
@@ -45,6 +52,7 @@ struct TimerBootCamp: View {
                 .minimumScaleFactor(0.1)
              */
             
+            
             // Count down
             /*
             Text(finishedText ?? "\(counter)")
@@ -54,16 +62,33 @@ struct TimerBootCamp: View {
                 .minimumScaleFactor(0.1)
              */
             
+            
             // Count down to date
+            /*
             Text(timeRemaining)
                 .font(.system(size: 100, weight: .semibold, design: .rounded))
                 .foregroundStyle(Color.white)
                 .lineLimit(1)
                 .minimumScaleFactor(0.1)
+            */
+            
+            
+            // Animation Counter
+            HStack(spacing: 15) {
+                Circle()
+                    .offset(y: count == 1 ? -20 : 0)
+                Circle()
+                    .offset(y: count == 2 ? -20 : 0)
+                Circle()
+                    .offset(y: count == 3 ? -20 : 0)
+            }
+            .frame(width: 200)
+            .foregroundStyle(Color.white)
         }
         .onReceive(timer) { value in
             // Current Time
             /* currentDate = value */
+            
             
             // Count down
             /*
@@ -75,11 +100,19 @@ struct TimerBootCamp: View {
             }
             */
             
+            
             // Count down to date
-            updateTimeRemaining()
+            /* updateTimeRemaining() */
+            
+            
+            // Animation Counter
+            withAnimation(.easeInOut(duration: 1.0)) {
+                count = count == 3 ? 0 : count+1
+            }
         }
     }
     
+    /*
     private func updateTimeRemaining() {
         let remaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: futureData)
         let hour = remaining.hour ?? 0
@@ -87,6 +120,7 @@ struct TimerBootCamp: View {
         let second = remaining.second ?? 0
         timeRemaining = "\(hour):\(minute):\(second)"
     }
+    */
 }
 
 #Preview {
