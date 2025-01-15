bb1ebfbce99ed5bc0e7e95120744119df7b8d7c3
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-15 16:57:27 +0530

    added the common Sequence operations in the publisher

--- SwiftConcurrencyBootCamp/Combine/Advanced Combine Pipelines/AdvancedCombineBootCamp.swift
+++ SwiftConcurrencyBootCamp/Combine/Advanced Combine Pipelines/AdvancedCombineBootCamp.swift
@@ -14,13 +14,17 @@ class AdvancedCombineDataService {
     }
     
     private func publishFakeData() {
-        let items = Array(0..<11)
+        let items = Array(1..<11)
         for x in items.indices {
             let sec = Double(x)
             DispatchQueue.main.asyncAfter(deadline: .now() + sec) {
 //                self.basicPublisher = items[x]
 //                self.currentValuePublisher.send(items[x])
                 self.passthroughPublisher.send(items[x])
+                
+                if x == items.indices.last {
+                    self.passthroughPublisher.send(completion: .finished)
+                }
             }
         }
     }
@@ -29,6 +33,7 @@ class AdvancedCombineDataService {
 class AdvancedCombineBootCampViewModel: ObservableObject {
     
     @Published var data: [String] = []
+    @Published var error: String = ""
     let dataService = AdvancedCombineDataService()
     var cancellables: Set<AnyCancellable> = []
     
@@ -38,13 +43,49 @@ class AdvancedCombineBootCampViewModel: ObservableObject {
     
     private func addSubscribers() {
         dataService.passthroughPublisher
+        
+        // Operations we can add to our publisher pipeline
+        
+        // Sequence operations
+        /*
+            // .first() // Only the first publisher coming through it will be published
+            // .first(where: { $0 > 4 }) // Can add conditions to what to publish
+            // .tryFirst(where: { value in
+            //     if value == 3 {
+            //         throw URLError(.badURL)
+            //     }
+            //     return value > 1
+            // })
+            // .last() // For last to work, it is mandatory for publisher to send the finished completion, like inside the publishFakeData() method in AdvancedCombineDataService
+            // .last(where: { $0 < 4 })
+            // .tryLast(where: { value in
+            //     if value == 13 {
+            //         throw URLError(.badURL)
+            //     }
+            //     return value > 1
+            // })
+            // .dropFirst() // This comes handly if the publisher is CurrentValueSubject, which has an initial value and we need to drop that value
+            // .dropFirst(3) // will drop the first 3 publishers
+            // .drop(while: { $0 < 5 })
+            // .tryDrop(while: { value in
+            //     if value == 15 {
+            //         throw URLError(.badURL)
+            //     }
+            //     return value < 6
+            // })
+            // .prefix(4) // will receive the first 4 publishers
+            // .output(at: 3) // output at one of the indexes
+            // .output(in: 3...5)
+            // .output(in: 2..<8)
+         */
+        
             .map({ String($0) })
             .sink { completion in
                 switch completion {
                 case .finished:
                     print("Finished")
                 case .failure(let error):
-                    print("Failed with error - \(error.localizedDescription)")
+                    self.error = "ERROR - \(error.localizedDescription)"
                 }
             } receiveValue: { [weak self] returnedValue in
                 self?.data.append(returnedValue)
@@ -65,6 +106,9 @@ struct AdvancedCombineBootCamp: View {
                     Text($0)
                         .font(.largeTitle)
                         .fontWeight(.black)
+                }
+                if !vm.error.isEmpty {
+                    Text(vm.error)
                 }
             }
         }
