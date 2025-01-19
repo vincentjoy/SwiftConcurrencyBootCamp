0d4f2e6e49707aded3adde947c5115afa945b9aa
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-19 22:10:45 +0530

    added custom key to property wrapper

--- SwiftConcurrencyBootCamp/Property Wrappers/PropertyWrapperBootCamp.swift
+++ SwiftConcurrencyBootCamp/Property Wrappers/PropertyWrapperBootCamp.swift
@@ -3,11 +3,11 @@
 import SwiftUI
 
 extension FileManager {
-    static func documensPath() -> URL {
+    static func documensPath(key: String) -> URL {
         FileManager.default
             .urls(for: .documentDirectory, in: .userDomainMask)
             .first!
-            .appending(path: "custom_file.txt")
+            .appending(path: "key.txt")
     }
 }
 
@@ -15,6 +15,8 @@ extension FileManager {
 struct FileManagerProperty: DynamicProperty {
     
     @State private var title: String
+    let key: String
+    
     var wrappedValue: String {
         get {
             title
@@ -23,9 +25,10 @@ struct FileManagerProperty: DynamicProperty {
         }
     }
     
-    init() {
+    init(key: String) {
+        self.key = key
         do {
-            title = try String(contentsOf: FileManager.documensPath(), encoding: .utf8)
+            title = try String(contentsOf: FileManager.documensPath(key: key), encoding: .utf8)
         } catch {
             title = "Starting Title"
             print("Error getting the title")
@@ -34,7 +37,7 @@ struct FileManagerProperty: DynamicProperty {
     
     func saveTitle(_ newTitle: String) {
         do {
-            try newTitle.write(to: FileManager.documensPath(), atomically: false, encoding: .utf8)
+            try newTitle.write(to: FileManager.documensPath(key: key), atomically: false, encoding: .utf8)
             title = newTitle
         } catch {
             print("Error saving the file: \(error.localizedDescription)")
@@ -44,16 +47,20 @@ struct FileManagerProperty: DynamicProperty {
 
 struct PropertyWrapperBootCamp: View {
     
-    @FileManagerProperty private var title: String
+    @FileManagerProperty(key: "custom_title_1") private var title1: String
+    @FileManagerProperty(key: "custom_title_2") private var title2: String
     
     var body: some View {
         VStack(spacing: 24) {
-            Text(title).font(.largeTitle)
+            Text(title1).font(.largeTitle)
+            Text(title2).font(.largeTitle)
             Button("Click me 1") {
-                title = "title 1"
+                title1 = "title 1"
+                title2 = "title 2"
             }
             Button("Click me 2") {
-                title = "title 2"
+                title1 = "custom title 1"
+                title2 = "custom title 2"
             }
         }
     }
